ebc214deb8c5dd73c8c39ad2fee72c35fbb5908d
sarvesh chaudhary <sarveshchaudhary@gmail.com>
2025-01-30 13:35:27 +0530

    commit template

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Insurance from "./home/insurance/page";
+export const metadata = {
+  title: "Insurance || Jano - Creative Multipurpose React NextJS Template",
+};
+const MainRoot = () => {
+  return <Insurance />;
+};
+
+export default MainRoot;
